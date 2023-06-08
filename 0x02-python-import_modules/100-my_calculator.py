0647f00f33970523585248d7d709d19a2d9da077
devcom33 <mouad.elb06@gmail.com>
2023-06-08 11:41:17 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add,sub,mul,div
     import sys
 
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -11,10 +13,10 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         if sys.argv[2] == '+':
-            print("{} + {} = {}".format(sys.argv[1], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
-            print("{} - {} = {}".format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]) ) ))
+            print("{} - {} = {}".format(a, b, sub(a, b) ))
         elif sys.argv[2] == '*':
-            print("{} * {} = {}".format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} * {} = {}".format(a, b, mul(a, b) ))
         elif sys.argv[2] == '/':
-            print("{} / {} = {}".format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} / {} = {}".format(a, b, div(a, b) ))
