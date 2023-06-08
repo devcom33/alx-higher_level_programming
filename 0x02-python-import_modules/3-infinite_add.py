ead7a9fc749c20856fd832ee7c3bb3e41716a377
devcom33 <mouad.elb06@gmail.com>
2023-06-08 10:22:00 -0700

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if "__name__" == __main__:
+    import sys
+    s = 0
+    if len(sys.argv) == 1:
+        pass
+    else:
+        for k in sys.argv[1:]:
+            s += int(k)
+    print(s)
