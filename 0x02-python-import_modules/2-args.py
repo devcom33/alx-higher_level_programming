c58cacbcc89ba52f2f4fb038045d54b006ee0751
devcom33 <mouad.elb06@gmail.com>
2023-06-08 09:33:33 -0700

    create new prog

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("{} argument:".format(len(sys.argv) - 1))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for k in sys.argv[1:]:
-        print(k)
+def dyn_args():
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for k in sys.argv[1:]:
+            print(k)
+if __name__ == "__main__":
+    dyn_args()
