5d97d46621c79184b737f1b0796dc99fdc916b8f
devcom33 <mouad.elb06@gmail.com>
2023-06-08 11:42:24 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add,sub,mul,div
+    from calculator_1 import add, sub, mul, div
     import sys
 
     a = int(sys.argv[1])
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         if sys.argv[2] == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
-            print("{} - {} = {}".format(a, b, sub(a, b) ))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif sys.argv[2] == '*':
-            print("{} * {} = {}".format(a, b, mul(a, b) ))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif sys.argv[2] == '/':
-            print("{} / {} = {}".format(a, b, div(a, b) ))
+            print("{} / {} = {}".format(a, b, div(a, b)))
