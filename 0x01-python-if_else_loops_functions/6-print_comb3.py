b64ef015142ffd1b7c58583ec5ecd16573fb14b7
devcom33 <mouad.elb06@gmail.com>
2023-06-09 04:43:19 -0700

    fix styles error and optimize the code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(1, 89):
-    if  int(str(i)[::-1]) >= i:
-        if i > 9:
-            if int(str(i)[::-1]) != i:
-                print("{:02d}, ".format(i), end="")
-        else:
-            print("{:02d}, ".format(i), end="")
+    if int(str(i)[::-1]) >= i and (i <= 9 or int(str(i)[::-1]) != i):
+        print("{:02d}, ".format(i), end="")
 print("{:d}".format(89))
