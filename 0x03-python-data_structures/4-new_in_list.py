cd1f26d8ac8959949046523984100dc41f70b15e
devcom33 <mouad.elb06@gmail.com>
2023-06-13 15:59:40 -0700

    copy of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    if not my_list:
+        my_list.copy()
     if idx < 0:
         return my_list.copy()
     if len(my_list) <= idx:
