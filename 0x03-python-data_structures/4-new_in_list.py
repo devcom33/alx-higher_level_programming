b756a723f7cc2a49033f6c0a3bb8bf40ee884f71
devcom33 <mouad.elb06@gmail.com>
2023-06-13 15:47:56 -0700

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if len(my_list) >= idx:
+        return my_list
+    my_list[idx] = element
+    return my_list
