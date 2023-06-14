40d5a72effb79ce859ffeea21101d3152fc662e9
devcom33 <mouad.elb06@gmail.com>
2023-06-14 10:54:45 -0700

    multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = list(map(lambda x, y: x * y, a_dictionary.values()))
+    new_dic = [list(map(lambda x, y: x * y, a_dictionary.values()))]
     return new_dic
