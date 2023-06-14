1c2b46a9586a91fa9433ac0953e53b92d1773a8b
devcom33 <mouad.elb06@gmail.com>
2023-06-14 11:11:21 -0700

    mul by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = []
+    new_dic = {}
     for i in a_dictionary:
-        new_dic = i * 2
+        new_dic.update({i: a_dictionary.get(i) * 2})
     return new_dic
