de358d2afc529563b3917d7ba85d45fa42e10b68
devcom33 <mouad.elb06@gmail.com>
2023-06-14 11:31:46 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not len(a_dictionary):
+    if not a_dictionary:
         return None
     x = a_dictionary.values()
     return list(a_dictionary.keys())[list(a_dictionary.values()).index(max(x))]
