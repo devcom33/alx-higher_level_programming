a2384ecacb77f919e225635495eb3b336f2ac1ce
devcom33 <mouad.elb06@gmail.com>
2023-06-14 12:19:28 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if !roman_string:
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    res = 0
+    if not roman_string or roman_string == "":
         return 0
+    if roman_string == "IX":
+        res = 9
+    elif roman_string == "IIV":
+        res = 4
+    else:
+        for i in roman_string:
+            res += dic[i]
+    return res
