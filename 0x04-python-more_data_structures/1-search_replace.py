efd085b4bd0617d442ebbedcf2a5db9ac16e066e
devcom33 <mouad.elb06@gmail.com>
2023-06-14 06:04:35 -0700

    fix all errors

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for i in my_list:
         if i == search:
-            my_list[my_list.index(i)] = replace
-    return my_list
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
