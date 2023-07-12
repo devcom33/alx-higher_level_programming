420c7bae53c0463e4e989ba2b6ef476b44d67ae6
devcom33 <mouad.elb06@gmail.com>
2023-07-11 17:28:34 -0700

    add .

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,8 @@
 """
 read_file func
 """
+
 def read_file(filename=""):
+    """read a txt (UTD-8) and print it to stdout"""
     with open(filename, "r", encoding="utf-8"):
         print(f.read(), end="")
