1d7306c77bb00710a5d6a97708ece0a7bb275b74
devcom33 <mouad.elb06@gmail.com>
2023-07-11 17:43:36 -0700

    add .

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ function wrtie_file
 
 
 def write_file(filename="", text=""):
-	"""return the number of ch written to filename from ..."""
-	with open(filename, 'w', encoding='utf=8') as f:
-		return f.write(text)
+    """return the number of ch written to filename from ..."""
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
