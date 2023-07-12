4a9c48ea8caf21ce2388b351ea80ef9d5c424e21
devcom33 <mouad.elb06@gmail.com>
2023-07-12 13:21:52 -0700

    py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """returns the number of characters added"""
-    with open(filename, "a", UTF-8) as f:
+    with open(filename, "a", "UTF-8") as f:
         f.write(text)
     return len(text)
