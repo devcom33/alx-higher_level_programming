5149194c82c8f0d438eba9fc8fbecadb8cd786ba
devcom33 <mouad.elb06@gmail.com>
2023-07-12 14:05:23 -0700

    json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that returns an object (Python data structure) represented by a JSON string"""
+
+
+def from_json_string(my_str):
+    """returns an object"""
+    return my_str.loads()
