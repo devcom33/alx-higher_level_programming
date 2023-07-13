f632fb77d667adbda5b478033ba756f3b3a6f22b
devcom33 <mouad.elb06@gmail.com>
2023-07-12 17:37:08 -0700

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """a function that returns the dictionary description with simple ds"""
 
+
 def class_to_json(obj):
+    """returns the dictionary description"""
     return type(obj)
