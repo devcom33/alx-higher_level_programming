c248030c1786aaa21e88af0f87f924722b3b435b
devcom33 <mouad.elb06@gmail.com>
2023-07-12 17:00:08 -0700

    add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and then save"""
+import sys
+import json
+import save_to_json_file
+import load_from_json_file
+
+def main():
+    """adds all arguments to a Python list"""
+    args = sys.argv[1:]
+    c = load_from_json_file(f)
+    c.append(args)
+    save_to_json_file(c)
+
+if __name__ == '__main__':
+    main()
