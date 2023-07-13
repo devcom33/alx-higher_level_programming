f3f9e4e97ef5dc436e64f236f2f3e7a5bb84759b
devcom33 <mouad.elb06@gmail.com>
2023-07-12 17:21:59 -0700

    solve import errs

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,7 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def main():
-    """adds all arguments to a Python list"""
-    args = sys.argv[1:]
-    c = load_from_json_file("add_item.json")
-    c.append(args)
-    save_to_json_file(c, "add_item.json")
-
+args = sys.argv[1:]
+c = load_from_json_file("add_item.json")
+c.append(args)
+save_to_json_file(c, "add_item.json")
