404b5ec92ba9f41b1f06369b78ca3f7d96ff3b08
devcom33 <mouad.elb06@gmail.com>
2023-07-12 17:07:43 -0700

    add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """adds all arguments to a Python list, and then save"""
 import sys
 import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 def main():
     """adds all arguments to a Python list"""
