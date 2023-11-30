625b5f0a2218bf4c9bc01b2e7c45d6fe53119928
devcom33 <mouad.elb06@gmail.com>
2023-11-30 03:37:22 -0800

    resolve Exception Error and change the or to and

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,10 +16,10 @@ def add_integer(a, b=98):
         >>> add_integer(-2, 2)
         0
     """
-    if type(a) is not int:
-        raise "a must be an integer"
-    if type(b) is not int:
-        raise "b must be an integer"
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
     return a + b
 
 
