474bd1795d24e44658a53c57b73588e7efb4be98
devcom33 <mouad.elb06@gmail.com>
2023-11-30 03:16:38 -0800

    add tests to txt file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,21 @@
     A function that adds 2 integers
 """
 def add_integer(a, b=98):
+    """
+        Add two integers
+        A function that takes two numbers, a and b returs sum
+
+        Usage:
+        >>> add_integer(4,7)
+        11
+        >>> add_integer(-2, 2)
+        0
+    """
     if type(a) is not int:
         raise "a must be an integer"
     if type(b) is not int:
         raise "b must be an integer"
-    if type(a) is not float:
-        raise "a must be an integer"
-    if type(b) is not float:
-        raise "b must be an integer"
     return a + b
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
