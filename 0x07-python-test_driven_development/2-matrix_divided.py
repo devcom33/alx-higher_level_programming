d47b2b24d9d37da9440ba11ffef1e7082ece67d8
devcom33 <mouad.elb06@gmail.com>
2023-12-07 01:20:48 -0800

    add comments

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 """A function that divides all elements of a matrix"""
 
 def matrix_divided(matrix, div):
+    """Divide Python Elements"""
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
