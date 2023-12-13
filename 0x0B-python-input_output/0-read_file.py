c382bfc638a7da12a43bbf2415dc4badf109408e
devcom33 <mouad.elb06@gmail.com>
2023-12-13 15:25:40 -0800

    module doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" a function that reads a text file"""
 def read_file(filename=""):
+    """prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
