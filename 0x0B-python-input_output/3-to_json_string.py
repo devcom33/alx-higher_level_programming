0018d482ad00a5349f8f52e132f0592de9e53caa
devcom33 <mouad.elb06@gmail.com>
2023-12-13 15:54:40 -0800

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a function that returns the JSON representation"""
+
+
+def to_json_string(my_obj):
+    """a function that returns the JSON representation
+        Args:
+            my_obj: an object
+        Returns:
+            String of object
+    """
+    return json.dumps(my_obj)
