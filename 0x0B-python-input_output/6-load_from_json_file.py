1d594f8e5a2467eca48610a5f51a2f8e32a53796
devcom33 <mouad.elb06@gmail.com>
2023-12-14 02:03:52 -0800

    loads from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that writes an Object to a text file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ loadfrom_json_file: create an Object from json file
+        Args:
+            my_obj: object
+            filename: name of the file
+    """
+    with open(filename, 'r') as file:
+        lines = file.read(filename)
+        json.loads(lines)
