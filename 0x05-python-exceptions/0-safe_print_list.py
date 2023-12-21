daa207330d550cbdf9df833f0ca7b65bc8ef5ec1
devcom33 <mouad.elb06@gmail.com>
2023-12-21 09:39:37 -0800

    add :x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        return int(''.join(map(str, my_list)))
+        return int(''.join(map(str, my_list[:x])))
     except Exception as e:
         print(e)
