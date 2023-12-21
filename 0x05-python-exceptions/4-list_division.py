6b425e2d2390188b54c001a1c64ce2c3ffa3ec91
devcom33 <mouad.elb06@gmail.com>
2023-12-21 14:37:36 -0800

    add zip func

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     newlist = []
-    for i, j in my_list_1, my_list_2:
+    for i, j in zip(my_list_1, my_list_2):
         try:
             div = i / j
         except TypeError:
