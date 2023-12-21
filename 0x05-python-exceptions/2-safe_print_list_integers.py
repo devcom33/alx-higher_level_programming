742e2aab5a7ff9ba69e1fdfa517dbbe1a9332b56
devcom33 <mouad.elb06@gmail.com>
2023-12-21 12:28:11 -0800

    fix pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if str(int(my_list[i])) == str(my_list[i]):
-                print("{:d}".format(my_list[i]), end = "")
+                print("{:d}".format(my_list[i]), end="")
                 c += 1
         except(IndexError, TypeError):
             break
