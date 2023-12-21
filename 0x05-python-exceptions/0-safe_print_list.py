7a50fe7b923ea7fa1381f281203f155c95f36049
devcom33 <mouad.elb06@gmail.com>
2023-12-21 08:56:39 -0800

    fix errs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in my_list[]:
+        for i in my_list:
             print("{:d}".format(i), end="")
         print("")
     except Exception as e:
