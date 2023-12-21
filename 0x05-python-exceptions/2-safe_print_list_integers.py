81991d1664d4aed47e5ce5fedabeb56058d3e931
devcom33 <mouad.elb06@gmail.com>
2023-12-21 12:41:34 -0800

    new opt

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,6 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-
     c = 0
     for i in range(x):
         try:
@@ -12,7 +11,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 c += 1
         except(IndexError, TypeError, ValueError):
             pass
-    if x > c:
-        raise Exception
     print("")
     return c
