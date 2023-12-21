8d9b36890e7a2b676647e1dee1d415e1dff5854a
devcom33 <mouad.elb06@gmail.com>
2023-12-21 10:01:16 -0800

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """a function that prints x elements of a list"""
 
-def list_size(my_list=[]):
-    c = 0
-    for i in my_list:
-        c = c + 1
-    return c
 
 def safe_print_list(my_list=[], x=0):
     try:
         print(int(''.join(map(str, my_list[:x]))), end='')
         print("")
     except:
-        print(x)
-    return list_size(my_list)
+        print(0)
+    return list_size(my_list, x)
+
+""" a function that counts"""
+
+
+def list_size(my_list=[], x=0):
+    c = 0
+    for i in my_list:
+        c = c + 1
+    if x <= c:
+        return x
+    return c
