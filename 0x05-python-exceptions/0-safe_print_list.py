aa616918bd32b0b11adbc304207e43084dcaeaf8
devcom33 <mouad.elb06@gmail.com>
2023-12-21 08:55:12 -0800

    create Readme and safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+"""a function that prints x elements of a list"""
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in my_list[]:
+            print("{:d}".format(i), end="")
+        print("")
+    except Exception as e:
+        print(e)
