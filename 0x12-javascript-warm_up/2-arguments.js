13d802d3cbc6c64b0d3aaa77e88deb258ed684c3
devcom33 <mouad.elb06@gmail.com>
2024-02-18 03:56:25 -0800

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const args = process.argv.slice(2);
 const numArgs = args.length;
 
 if (numArgs === 0) {
-  console.log('No Argument');
+  console.log('No Argument\n');
 } else if (numArgs === 1) {
-  console.log('Argument found');
+  console.log('Argument found\n');
 } else {
-  console.log('Arguments found');
+  console.log('Arguments found\n');
 }
