953459af13e3205d10ec66514de4393c9d389590
devcom33 <mouad.elb06@gmail.com>
2024-04-11 05:32:43 -0700

    hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,26 @@
 """a script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb, sys
 
-db = MySQLdb.connect(host="localhost", port = 3306, sys.argv[1], sys.argv[2], sys.argv[3])
 
-cur = db.cursor()
 
-numrows = cur.execute("SELECT * from {}".format(sys.argv[3]))
+def list_states(username, passwd, db):
+    """
+    Mysql Connection & lists all states
+    """
+    db = MySQLdb.connect(host="localhost", port = 3306, username = username, password = passwd, database = db)
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * from {}".format(db))
+    print("Num Rows :", numrows)
 
-print("Num Rows :", numrows)
+    cursor.close()
+
+if __name__ == "__main__":
+    #check args len
+    if (len(sys.argv)) != 4:
+            sys.exit(1)
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    list_states(username, passwd, db)
