a6442c32455d756f2ae77ba198649878b8dc7574
devcom33 <mouad.elb06@gmail.com>
2024-04-11 08:06:30 -0700

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def list_states_N(username, passwd, db, name):
+    """
+    Mysql Connection & lists all states
+    """
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=passwd,
+            database=db)
+    cur = db.cursor()
+    cur.execute("SELECT * from states\
+            WHERE name LIKE BINARY 'N%' AND name={} ORDER BY states.id asc".format(name))
+    states = cur.fetchall()
+
+    for row in states:
+        print(row)
+
+    cur.close()
+
+
+if __name__ == "__main__":
+    """check args len"""
+    if (len(sys.argv)) != 5:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4]
+
+    list_states_N(username, passwd, db, name)
